e26b55e7376119b6b7a8438b45187ff095a4ada7
bilal elsayed <bilalelsayed97@gmail.com>
2024-02-22 15:14:34 +0200

    Initial Commit

---
+++ Dice-SwiftUI/Dice_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dice_SwiftUIApp.swift
+//  Dice-SwiftUI
+//
+//  Created by bilal on 22/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct Dice_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
