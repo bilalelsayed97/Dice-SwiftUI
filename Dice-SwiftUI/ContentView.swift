4429509a7ff051c27a717a5452bd0409f3c5683a
bilal elsayed <bilalelsayed97@gmail.com>
2024-02-22 21:01:08 +0200

    completed ui/logic using Swift UI

--- Dice-SwiftUI/ContentView.swift
+++ Dice-SwiftUI/ContentView.swift
@@ -8,19 +8,47 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var n1: Int = 1
+    @State var n2: Int = 1
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            Image("background")
+                .resizable()
+                .edgesIgnoringSafeArea(.all)
+            VStack {
+                Image("diceeLogo")
+                Spacer()
+                HStack {
+                    DiceView(diceNum: n1).padding(.all)
+                    DiceView(diceNum: n2).padding(.all)
+                }
+                .padding(.all)
+                Spacer()
+                Button {
+                    self.n1 = Int.random(in: 1...6)
+                    self.n2 = Int.random(in: 1...6)
+                } label: {
+                    HStack {
+                        Image(systemName: "dice.fill")
+                        Text("Roll").font(.system(size: 28))
+                    }.padding(.all).padding(.horizontal)
+                }
+                .background(Color(UIColor(red: 0.5, green: 0.7, blue: 0.2, alpha: 1.0)))
+                .foregroundColor(.white).cornerRadius(50)
+            }
         }
-        .padding()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct DiceView: View {
+var diceNum: Int
+    var body: some View {
+        Image("dice\(diceNum)").resizable().aspectRatio(contentMode: .fit).padding(.all)
     }
 }
